cebaf56fd3e72ecb7bf1b3574e70a6a56a268c0a
Shah Rukh Khan <shahrukh@linkgraph.io>
2022-12-23 16:48:13 +0500

    added field name and email

--- users/models.py
+++ users/models.py
@@ -7,4 +7,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(max_length=40, null=True, blank=True)
-
+    email = models.EmailField(max_length=40, null=True, blank=True)
