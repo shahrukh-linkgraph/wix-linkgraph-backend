d9affd88e8d26c58b8695f94612aaeacf47869c8
shah Rukh Khan <shah@crowdbotics.com>
2023-02-02 18:20:55 +0500

    Setup new DB Postgres

--- linkgraph_plugin/settings.py
+++ linkgraph_plugin/settings.py
@@ -101,13 +101,10 @@ REST_FRAMEWORK = {
 }
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env.str("DATABASE_URL", default=None):
+    DATABASES = {
+        'default': env.db_url(engine='django.db.backends.postgresql')
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
